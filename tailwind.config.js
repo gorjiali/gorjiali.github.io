6eff733fc93faf75bc5839277609badb390ca5fc
Ali Gorji <ag.aligorji@gmail.com>
2023-01-05 01:22:51 +0330

    feat: add base style with tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -6,5 +8,27 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        body: {
+          color: theme('colors.gray.700'),
+        },
+        a: {
+          color: theme('colors.gray.500'),
+          textDecoration: 'underline',
+          textDecorationColor: theme('colors.gray.300'),
+          textUnderlineOffset: theme('spacing.1'),
+          '&:hover': {
+            color: theme('colors.gray.600'),
+            textDecorationColor: theme('colors.gray.400'),
+          },
+        },
+        p: {
+          lineHeight: theme('lineHeight.7'),
+          marginBottom: theme('spacing.4'),
+        },
+      });
+    }),
+  ],
 };
